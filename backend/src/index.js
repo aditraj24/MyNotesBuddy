f3f6d29dcf4ff559dc90b50e5a46bc3cfb8f3635
aditraj24 <adityarajykme24@gmail.com>
2025-12-19 20:11:52 +0530

    fixing2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,43 @@
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import { app } from "./app.js";
+import express from "express";
+import cors from "cors";
 
-dotenv.config({
-  path: "./.env",
+import rateLimiter from "./middlewares/rateLimiter.middleware.js";
+import path from "path";
+import notesRoutes from "./routes/note.routes.js";
+
+dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json());
+app.use(rateLimiter);
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/notes", notesRoutes);
+
+// production frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB()
   .then(() => {
